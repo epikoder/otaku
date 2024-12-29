5d2d45c37f048a4ec121ecc620b002b1f689ace1
epikoder <efedua.bell@gmail.com>
2024-12-29 02:59:51 +0100

    log

---
+++ @types/model.d.ts
@@ -0,0 +1,5 @@
+interface Journal {
+
+}
+
+interface Chat {}
---
+++ utils/storage.ts
@@ -0,0 +1,25 @@
+interface Schema<T> {
+    id: string;
+    value: {
+        data: T;
+        timestamp: Date;
+    };
+}
+
+export abstract class DataStore<T> {
+    constructor(db: string) {
+        this._db = db
+    }
+
+    private _db:string;
+    async get(id: string): Promise<T | undefined> {
+        
+        return;
+    }
+    async set(id: string, value: T): Promise<void> {}
+    async update(id: string, value: T): Promise<void> {}
+    async delete(id: string): Promise<T | undefined> {
+        return;
+    }
+    private async sync() {}
+}
